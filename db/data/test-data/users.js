34289d18101f22140cb6579f3d84f1d5908c91a0
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-03-03 16:52:39 +0000

    add users test data

---
+++ db/data/test-data/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    username: "badmanting",
+    hash: "hash1",
+  },
+  {
+    username: "madDog123",
+    hash: "hash2",
+  },
+  {
+    username: "pikachu457",
+    hash: "hash3",
+  },
+  {
+    username: "naruto",
+    hash: "hash4",
+  },
+  {
+    username: "naruto",
+    hash: "hash4"
+  }
+];
