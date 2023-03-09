73a2baecb6473330989859514fdd98ff9bb702e1
NuclearEngineer1 <ljk8@hotmail.co.uk>
2023-03-09 16:59:07 +0000

    finish seed file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -17,14 +17,16 @@ const seed = async ({ sightingData, commentData, userData }) => {
   await db.query(
     `CREATE TABLE comments (
       comment_id SERIAL PRIMARY KEY,
+      sighting_id INT NOT NULL REFERENCES sightings(sighting_id),
+      body VARCHAR NOT NULL,
       author VARCHAR NOT NULL REFERENCES users(username),
-      hash VARCHAR NOT NULL
+      created_at TIMESTAMP DEFAULT NOW()
     )`
   );
 
   await db.query(
     `CREATE TABLE sightings (
-      spotting_id SERIAL PRIMARY KEY,
+      sighting_id SERIAL PRIMARY KEY,
       species VARCHAR NOT NULL,
       lat FLOAT NOT NULL,
       long FLOAT NOT NULL,
@@ -39,17 +41,54 @@ const seed = async ({ sightingData, commentData, userData }) => {
   const insertUsersQueryStr = format(
     `INSERT INTO users (username, hash) VALUES %L RETURNING *`,
     userData.map(({ username, hash }) => {
-      return [
-        username,
-        hash
-      ];
+      return [username, hash];
     })
   );
 
-  const formattedCommentData = commentData.map(convertTimeStampToDate)
+  const formattedCommentData = commentData.map(convertTimeStampToDate);
 
-  const formattedSightingData = sightingData.map(convertTimeStampToDate)
+  const formattedSightingData = sightingData.map(convertTimeStampToDate);
 
-  
+  const insertCommentsQueryStr = format(
+    `INSERT INTO comments (sighting_id, body, author, created_at) VALUES %L RETURNING *`,
+    commentData.map(({ sighting_id, body, author, created_at }) => {
+      return [sighting_id, body, author, created_at];
+    })
+  );
 
+  const insertSightingsQueryStr = format(
+    `INSERT INTO sightings (species, lat, long, date_spotted, date_submitted, author, img_base64, description) VALUES %L RETURNING *`,
+    sightingData.map(
+      ({
+        species,
+        lat,
+        long,
+        date_spotted,
+        date_submitted,
+        author,
+        img_base64,
+        description,
+      }) => {
+        return [
+          species,
+          lat,
+          long,
+          date_spotted,
+          date_submitted,
+          author,
+          img_base64,
+          description,
+        ];
+      }
+    )
+  );
+
+  await db.query(insertUsersQueryStr);
+
+  await db.query(insertSightingsQueryStr);
+
+  await db.query(insertCommentsQueryStr);
 };
+
+module.exports = seed;
+
